cbdd7d01da80d25078a7ae17eb94ede4ecb0fe7f
Mukhopadhyay <praneshmukherjee7@gmail.com>
2022-02-15 00:04:11 +0530

    Adding all url tests (except csv file)

--- tests.py
+++ tests.py
@@ -1,13 +1,16 @@
 import os
 import json
+from turtle import clear
 import pytest
 import asyncio
 import aiohttp
+import warnings
 from os import path
 import pandas as pd
 import utils.utils as utils
 import utils.config as config
 
+warnings.filterwarnings('ignore')
 
 def test_data_files() -> None:
     data_dir_contents = os.listdir('data')
@@ -58,8 +61,14 @@ def test_create_openweb_markdown_table() -> None:
 ###################################
 
 async def fetch(session, url):
+    """
+    For now considering the followng status code
+    
+    200: The HTTP 200 OK success status response code indicates that the request has succeeded
+    403: The HTTP 403 Forbidden response status code indicates that the server understands the request but refuses to authorize it.
+    """
     async with session.get(url) as response:
-        assert response.status == 200
+        assert response.status in [200, 403]
         result = await response.text(), response.status, url
         return result
 
@@ -71,6 +80,18 @@ async def fetch_all(session, urls):
     result = await asyncio.gather(*tasks)
     return result
 
+async def audio_urls():
+    with open(path.join(config.DATA_DIR, config.AUDIO_DATASETS), 'r') as file:
+        content = json.load(file)
+        urls = [_['url'] for _ in content]
+    
+    async with aiohttp.ClientSession() as session:
+        results = await fetch_all(session, urls)
+        verbose = f'\n\t{"*"*40}\n\t AUDIO URLS\n\t{"*"*40}'
+        print(verbose)
+        for _, code, url in results:
+            print(f'\t\t{url} ({code})')
+
 async def image_urls():
     with open(path.join(config.DATA_DIR, config.IMAGE_DATASETS), 'r') as file:
         content = json.load(file)
@@ -78,9 +99,64 @@ async def image_urls():
 
     async with aiohttp.ClientSession() as session:
         results = await fetch_all(session, urls)
-        print('\n\t', '*'*40, '\n\t Image URLS\n\t', '*'*40)
+        verbose = f'\n\t{"*"*40}\n\t IMAGE URLS\n\t{"*"*40}'
+        print(verbose)
         for _, code, url in results:
             print(f'\t\t{url} ({code})')
 
+async def nlp_urls():
+    with open(path.join(config.DATA_DIR, config.NLP_DATASETS), 'r') as file:
+        content = json.load(file)
+        urls = [_['url'] for _ in content]
+    
+    async with aiohttp.ClientSession() as session:
+        results = await fetch_all(session, urls)
+        verbose = f'\n\t{"*"*40}\n\t NLP URLS\n\t{"*"*40}'
+        print(verbose)
+        for _, code, url in results:
+            print(f'\t\t{url} ({code})')
+
+async def open_gov_add_urls():
+    with open(path.join(config.DATA_DIR, config.OPEN_GOV_ADDITIONAL), 'r') as file:
+        content = json.load(file)
+        urls = [_['url'] for _ in content]
+    
+    async with aiohttp.ClientSession() as session:
+        results = await fetch_all(session, urls)
+        verbose = f'\n\t{"*"*40}\n\t OPEN-GOV ADDITIONAL URLS\n\t{"*"*40}'
+        print(verbose)
+        for _, code, url in results:
+            print(f'\t\t{url} ({code})')
+
+async def opendata_website_urls():
+    with open(path.join(config.DATA_DIR, config.OPEN_DATA_WEBSITES), 'r') as file:
+        content = json.load(file)
+        urls = [_['url'] for _ in content]
+    
+    async with aiohttp.ClientSession() as session:
+        results = await fetch_all(session, urls)
+        verbose = f'\n\t{"*"*40}\n\t OPENDATA-WEB URLS\n\t{"*"*40}'
+        print(verbose)
+        for _, code, url in results:
+            print(f'\t\t{url} ({code})')
+
+
+def test_audio_urls():
+    asyncio.run(audio_urls())
+
+
 def test_image_urls():
     asyncio.run(image_urls())
+
+
+def test_nlp_urls():
+    asyncio.run(nlp_urls())
+
+
+def test_open_gov_additional_urls():
+    asyncio.run(open_gov_add_urls())
+
+
+def test_opendata_web_urls():
+    asyncio.run(opendata_website_urls())
+
